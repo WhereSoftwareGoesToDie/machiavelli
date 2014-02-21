400de86d3318d4149142a50e7ff5a7e854f1d196
Katie McLaughlin <katie@anchor.net.au>
2014-02-21 15:51:39 +1100

    update to forked version from glasnt/cubism
    use version from branch axis_24-hour-time to change ruler to show 24-hour time

--- vendor/assets/javascripts/cubism.v1.js
+++ vendor/assets/javascripts/cubism.v1.js
@@ -1,9 +1,5 @@
-// Cubism.js customized
-// 
-// MOD20140214KM - remove any %p formatting styles to create 24hour time ruler
-
 (function(exports){
-var cubism = exports.cubism = {version: "1.3.0"};
+var cubism = exports.cubism = {version: "1.5.0"};
 var cubism_id = 0;
 function cubism_identity(d) { return d; }
 cubism.option = function(name, defaultValue) {
@@ -189,6 +185,210 @@ cubism_contextPrototype.cube = function(host) {
 };
 
 var cubism_cubeFormatDate = d3.time.format.iso;
+/* librato (http://dev.librato.com/v1/post/metrics) source
+ * If you want to see an example of how to use this source, check: https://gist.github.com/drio/5792680
+ */
+cubism_contextPrototype.librato = function(user, token) {
+  var source      = {},
+      context     = this;
+      auth_string = "Basic " + btoa(user + ":" + token);
+      enable_log  = true,
+      avail_rsts  = [ 1, 60, 900, 3600 ];
+
+  function log(msg) { if (enable_log) console.log(msg); }
+
+  function log_interval(start, stop, step) {
+    log("---------------------- Starting request");
+    log("START: " + new Date(start * 1000));
+    log("STOP : " + new Date(stop * 1000));
+    log("STEP : " + step);
+    var nes_num_mes = (stop - start)/step;
+    log("# of measurements necessary: " + nes_num_mes);
+  }
+
+  /* Given a step, find the best librato resolution to use.
+   *
+   * Example:
+   *
+   * (s) : cubism step
+   *
+   * avail_rsts   1 --------------- 60 --------------- 900 ---------------- 3600
+   *                                |    (s)            |
+   *                                |                   |
+   *                              [low_res             top_res]
+   *
+   * return: low_res (60)
+   */
+  function find_ideal_librato_resolution(step) {
+    var highest_res = avail_rsts[0],
+        lowest_res  = avail_rsts[avail_rsts.length]; // high and lowest available resolution from librato
+
+    /* If step is outside the highest or lowest librato resolution, pick them and we are done */
+    if (step >= lowest_res)
+      return lowest_res;
+
+    if (step <= highest_res)
+      return highest_res;
+
+    /* If not, find in what resolution interval the step lands. */
+    var iof, top_res, i;
+    for (i=step; i<=lowest_res; i++) {
+      iof = avail_rsts.indexOf(i);
+      if (iof > -1) {
+        top_res = avail_rsts[iof];
+        break;
+      }
+    }
+
+    var low_res;
+    for (i=step; i>=highest_res; i--) {
+      iof = avail_rsts.indexOf(i);
+      if (iof > -1) {
+        low_res = avail_rsts[iof];
+        break;
+      }
+    }
+
+    /* What's the closest librato resolution given the step ? */
+    return ((top_res-step) < (step-low_res)) ? top_res : low_res;
+  }
+
+  function find_librato_resolution(sdate, edate, step) {
+    var i_size      = edate - sdate,                 // interval size
+        month       = 2419200,
+        week        = 604800,
+        two_days    = 172800,
+        ideal_res;
+
+    if (i_size > month)
+      return 3600;
+
+    ideal_res = find_ideal_librato_resolution(step);
+
+    /*
+     * Now we have the ideal resolution, but due to the retention policies at librato, maybe we have
+     * to use a higher resolution.
+     * http://support.metrics.librato.com/knowledgebase/articles/66838-understanding-metrics-roll-ups-retention-and-grap
+     */
+    if (i_size > week && ideal_res < 900)
+      return 900;
+    else if (i_size > two_days && ideal_res < 60)
+      return 60;
+    else
+      return ideal_res;
+  }
+
+  /* All the logic to query the librato API is here */
+  var librato_request = function(metric, source) {
+    var url_prefix  = "https://metrics-api.librato.com/v1/metrics";
+
+    function make_url(sdate, edate, step) {
+      var params    = "start_time="  + sdate +
+                      "&end_time="   + edate +
+                      "&resolution=" + find_librato_resolution(sdate, edate, step);
+          full_url  = url_prefix + "/" + metric + "?" + params;
+
+      log("full_url = " + full_url);
+      log_interval(sdate, edate, step);
+      return full_url;
+    }
+
+    /*
+     * We are most likely not going to get the same number of measurements
+     * cubism expects for a particular context: We have to perform down/up
+     * sampling
+     */
+    function down_up_sampling(isdate, iedate, step, librato_mm) {
+      var av = [];
+
+      for (i=isdate; i<=iedate; i+=step) {
+        var int_mes = [];
+        while (librato_mm.length && librato_mm[0].measure_time <= i) {
+          int_mes.push(librato_mm.shift().value);
+        }
+
+        var v;
+        if (int_mes.length) { /* Compute the average */
+          v = int_mes.reduce(function(a, b) { return a + b }) / int_mes.length;
+        } else { /* No librato values on interval */
+          v = (av.length) ? av[av.length-1] : 0;
+        }
+        av.push(v);
+      }
+
+      return av;
+    }
+
+    request = {};
+
+    request.fire = function(isdate, iedate, step, callback_done) {
+      var a_values = []; /* Store partial values from librato */
+
+      /*
+       * Librato has a limit in the number of measurements we get back in a request (100).
+       * We recursively perform requests to the API to ensure we have all the data points
+       * for the interval we are working on.
+       */
+      function actual_request(full_url) {
+        d3.json(full_url)
+          .header("X-Requested-With", "XMLHttpRequest")
+          .header("Authorization", auth_string)
+          .header("Librato-User-Agent", 'cubism/' + cubism.version)
+          .get(function (error, data) { /* Callback; data available */
+            if (!error) {
+              log("# of partial measurements: " + data.measurements[source].length);
+              data.measurements[source].forEach(function(o) { a_values.push(o); });
+
+              var still_more_values = 'query' in data && 'next_time' in data.query;
+              if (still_more_values) {
+                log("Requesting more values");
+                actual_request(make_url(data.query.next_time, iedate, step));
+              } else {
+                log("total number of measurements from librato: " + a_values.length);
+                var a_adjusted = down_up_sampling(isdate, iedate, step, a_values);
+                log("number of measurements after adjusting time values: " + a_adjusted.length);
+                callback_done(a_adjusted);
+              }
+            } else {
+              log("There was an error when performing the librato request:");
+              log(error);
+            }
+          });
+      }
+
+      actual_request(make_url(isdate, iedate, step));
+    };
+
+    return request;
+  };
+
+  /*
+   * The user will use this method to create a cubism source (librato in this case)
+   * and call .metric() as necessary to create metrics.
+   */
+  source.metric = function(m_name, m_source) {
+    return context.metric(function(start, stop, step, callback) {
+      /* All the librato logic is here; .fire() retrieves the metrics' data */
+      librato_request(m_name, m_source)
+        .fire(cubism_libratoFormatDate(start),
+              cubism_libratoFormatDate(stop),
+              cubism_libratoFormatDate(step),
+              function(a_values) { callback(null, a_values); });
+
+      }, m_name += "");
+    };
+
+  /* This is not used when the source is librato */
+  source.toString = function() {
+    return "librato";
+  };
+
+  return source;
+};
+
+var cubism_libratoFormatDate = function(time) {
+  return Math.floor(time / 1000);
+};
 cubism_contextPrototype.graphite = function(host) {
   if (!arguments.length) host = "";
   var source = {},
@@ -205,19 +405,11 @@ cubism_contextPrototype.graphite = function(host) {
           + (!(step % 36e5) ? step / 36e5 + "hour" : !(step % 6e4) ? step / 6e4 + "min" : step / 1e3 + "sec")
           + "','" + sum + "')";
 
-	url = host + "/render?format=raw"
-          + "&target=" + encodeURIComponent("alias(" + target + ",'')")
-          + "&from=" + cubism_graphiteFormatDate(start - 2 * step) // off-by-two?
-          + "&until=" + cubism_graphiteFormatDate(stop - 1000)
-
-	console.log("GRAPHITE URL :"+url)
-
       d3.text(host + "/render?format=raw"
           + "&target=" + encodeURIComponent("alias(" + target + ",'')")
           + "&from=" + cubism_graphiteFormatDate(start - 2 * step) // off-by-two?
           + "&until=" + cubism_graphiteFormatDate(stop - 1000), function(text) {
         if (!text) return callback(new Error("unable to load data"));
-	console.log("graphite returned data: "+text)
         callback(null, cubism_graphiteParse(text));
       });
     }, expression += "");
@@ -260,17 +452,12 @@ function cubism_graphiteParse(text) {
       a = meta.lastIndexOf(",", b - 1),
       start = meta.substring(a + 1, b) * 1000,
       step = meta.substring(c + 1) * 1000;
-
-result = text
-          .substring(i + 1)                                                                                                                                                                   .split(",")                                                                                                                                                                         .slice(1) 
-
-          console.log("graphite parse result "+result)
-          // the first value is always None?  
-          return result.map(function(d) { return +d; });
-
+  return text
+      .substring(i + 1)
+      .split(",")
+      .slice(1) // the first value is always None?
+      .map(function(d) { return +d; });
 }
-
-
 cubism_contextPrototype.gangliaWeb = function(config) {
   var host = '',
       uriPathPrefix = '/ganglia2/';
@@ -1015,9 +1202,10 @@ cubism_contextPrototype.axis = function() {
       scale = context.scale,
       axis_ = d3.svg.axis().scale(scale);
 
-  var format = context.step() < 6e4 ? cubism_axisFormatSeconds
+  var formatDefault = context.step() < 6e4 ? cubism_axisFormatSeconds
       : context.step() < 864e5 ? cubism_axisFormatMinutes
       : cubism_axisFormatDays;
+  var format = formatDefault;
 
   function axis(selection) {
     var id = ++cubism_id,
@@ -1064,6 +1252,12 @@ cubism_contextPrototype.axis = function() {
     }
   };
 
+  axis.focusFormat = function(_) {
+    if (!arguments.length) return format == formatDefault ? null : _;
+    format = _ == null ? formatDefault : _;
+    return axis;
+  };
+
   return d3.rebind(axis, axis_,
       "orient",
       "ticks",
@@ -1073,8 +1267,8 @@ cubism_contextPrototype.axis = function() {
       "tickFormat");
 };
 
-var cubism_axisFormatSeconds = d3.time.format("%H:%M:%S"), //MOD20140214KM
-    cubism_axisFormatMinutes = d3.time.format("%H:%M"), //MOD20040214KM
+var cubism_axisFormatSeconds = d3.time.format("%H:%M:%S"),
+    cubism_axisFormatMinutes = d3.time.format("%H:%M"),
     cubism_axisFormatDays = d3.time.format("%B %d");
 cubism_contextPrototype.rule = function() {
   var context = this,
