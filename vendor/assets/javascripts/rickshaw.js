65c77f2410382a58ecea1cde1a37da9a977597a3
Katie McLaughlin <katie@anchor.net.au>
2014-02-27 09:48:26 +1100

    persistent zooming for rickshaw sliders
    using the [glasnt/rickshaw persisent_zoom] branch code, make the jquery range
    slider store it's handle values in the window.location.hash. Pick up these
    values, if available, on build.
    
    For adding/removing metrics on the same graph, append the hash on click to the
    sidebar urls. Do not append for changing graph type or duration, since
    persistent zooms aren't logical there.
    
    Allows adding/removing metrics to keep the same zoom. Allows url sharing of
    pre-zoom.
    
    Limitation: zoom is in seconds since epoch, so over time the zoom won't have
    any effect. Could add functionality to check that values are in range.

--- vendor/assets/javascripts/rickshaw.js
+++ vendor/assets/javascripts/rickshaw.js
@@ -2414,6 +2414,7 @@ Rickshaw.Graph.RangeSlider = Rickshaw.Class.create({
 
 		var element = this.element;
 		var graph = this.graph;
+		hash = window.location.hash.slice(1).split(",")
 
 		var self = this;
 
@@ -2424,8 +2425,8 @@ Rickshaw.Graph.RangeSlider = Rickshaw.Class.create({
 					min: graph[0].dataDomain()[0],
 					max: graph[0].dataDomain()[1],
 					values: [
-						graph[0].dataDomain()[0],
-						graph[0].dataDomain()[1]
+						hash[0] || graph[0].dataDomain()[0],
+						hash[1] || graph[0].dataDomain()[1]
 					],
 					slide: function( event, ui ) {
 						for( var i=0; i<graph.length; i++) {
@@ -2460,8 +2461,8 @@ Rickshaw.Graph.RangeSlider = Rickshaw.Class.create({
 					min: domain[0],
 					max: domain[1],
 					values: [ 
-						domain[0],
-						domain[1]
+						hash[0] || domain[0],
+						hash[1] || domain[1]
 					],
 					slide: function( event, ui ) {
 
@@ -2517,6 +2518,7 @@ Rickshaw.Graph.RangeSlider = Rickshaw.Class.create({
 			values[1] = domain[1];
 		}
 
+		window.location.hash = values
 		$(element).slider('option', 'values', values);
 	},
 
