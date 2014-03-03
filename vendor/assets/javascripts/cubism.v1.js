a12bfcbb8a0905c4f0b8f18c879f12b6c982fe1c
Katie McLaughlin <katie@anchor.net.au>
2014-03-03 11:45:50 +1100

    Update cubism.js to machiavelli version
    See [glasnt/cubism:machiavelli_context]

--- vendor/assets/javascripts/cubism.v1.js
+++ vendor/assets/javascripts/cubism.v1.js
@@ -458,6 +458,38 @@ function cubism_graphiteParse(text) {
       .slice(1) // the first value is always None?
       .map(function(d) { return +d; });
 }
+cubism_contextPrototype.machiavelli= function(host) {
+  if (!arguments.length) host = "";
+  var source = {},
+      context = this;
+
+  source.metric = function(target) {
+
+    var metric = context.metric(function(start, stop, step, callback) {
+	feed = host + "/metrics/"
+	      + target
+	      + "?start=" + cubism_machiavelliFormatDate(start - 2 * step)
+	      + "&end=" + cubism_machiavelliFormatDate(stop - 1000)
+	      + "&step="+ step/1000
+	d3.json(feed
+          , function(data) {
+          if (!data) return callback(new Error("unable to load data"));
+          callback(null, data.map(function(d) { return d.y} ))
+      });
+    }, target += "");
+    return metric;
+  };
+
+  source.toString = function() {
+    return host;
+  };
+
+  return source;
+};
+
+function cubism_machiavelliFormatDate(time) {                                                                                                                                        
+  return Math.floor(time / 1000);
+}
 cubism_contextPrototype.gangliaWeb = function(config) {
   var host = '',
       uriPathPrefix = '/ganglia2/';
