0daf32a9bebf829439f39793322d95d3d1d01f48
Katie McLaughlin <katie@anchor.net.au>
2014-03-04 10:32:38 +1100

    Refine rickshaw logic for fixed-duration charts
    Use the initial datapull for creating the graph, then add data per the helper
    function in the graph itself. maxpoints and startdate based on initial
    datapull.

--- app/assets/javascripts/graphs/rickshaw.js
+++ app/assets/javascripts/graphs/rickshaw.js
@@ -1,108 +1,60 @@
 var graph=[]
-
-
-var data; 
-function render_rickshaw(index, data) { 
-	chart = "chart_"+index
-	yaxis = "y_axis_"+index
-
-	graph[index]= new Rickshaw.Graph({
-                element: document.getElementById(chart),
-                width: 700,
-                height: 200,
-                renderer: 'line',
-                series: [{ color: "#cb513a", data: data }]
-        });
-
-        new Rickshaw.Graph.Axis.Y( {
-                graph: graph[index],
-                orientation: 'left',
-                tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-                element: document.getElementById(yaxis) 
-
-        } );
-
-        new Rickshaw.Graph.Axis.Time({
-                graph: graph[index],
-                timeFixture: new Rickshaw.Fixtures.Time.Local()
-        });
-
-        ///////
-        graph[index].render();
-
-        new Rickshaw.Graph.HoverDetail({ 
-                graph: graph[index],
-                formatter: function (series, x, y) {
-                        return content = "<span class='date'>"+d3.time.format("%Y-%m-%d %H:%M:%S")(new Date(x*1000)) +"</span><br/>"+y
-                }
-	});
-
-
-	unrenderWaiting();
-	renderSlider();
-
-}
-
+var data;
 
 function render_rickshaw(index) { 
-	graph[index] = new Rickshaw.Graph({
-		element: document.getElementById("chart_"+index),
-		width: 700,
-		height: 200,
-		renderer: 'line',
-		series: new Rickshaw.Series.FixedDuration([{name: "data"}], undefined, { 
-			timeInterval: gon.step*1000,
-			maxDataPoints: 200, 
-			timeBase: parseInt(new Date().getTime() / 1000)
-		})
-	})
 
-	
-        chart = "chart_"+index
-        yaxis = "y_axis_"+index
-
-	 new Rickshaw.Graph.Axis.Y( {
-                graph: graph[index],
-                orientation: 'left',
-                tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-                element: document.getElementById(yaxis)
-
-        } );
-
-        new Rickshaw.Graph.Axis.Time({
-                graph: graph[index],
-                timeFixture: new Rickshaw.Fixtures.Time.Local()
-        });
-
-
-
-	graph[index].render()
-
-	
-        new Rickshaw.Graph.HoverDetail({
-                graph: graph[index],
-                formatter: function (series, x, y) {
-                        return content = "<span class='date'>"+d3.time.format("%Y-%m-%d %H:%M:%S")(new Date(x*1000)) +"</span><br/>"+y
-                }
-        });
-
-
-	
 	update = gon.feed[index]+"?start="+gon.start+"&stop="+gon.stop+"&step="+gon.step                               
-                                                                                                                       
+                
         console.log(update)                                                                                            
-	$.getJSON(update, function(d) {                               
-                $.each(d, function(i, point) {                                                                         
-                        x = {data: point.y} 
-                        graph[index].series.addData(x) //{name: gon.metric[index], data: point.y})
-                })
-                graph[index].render()
+	$.getJSON(update, function(data) {
+
+		graph[index] = new Rickshaw.Graph({
+			element: document.getElementById("chart_"+index),
+			width: 700,
+			height: 200,
+			renderer: 'line',
+			series: new Rickshaw.Series.FixedDuration([{name: "data" }], undefined, { 
+				timeInterval: gon.step*1000,
+				maxDataPoints: data.length, 
+				timeBase: data[0].x  
+			})
+		})
+
+		chart = "chart_"+index
+		yaxis = "y_axis_"+index
+
+		new Rickshaw.Graph.Axis.Y( {
+			graph: graph[index],
+			orientation: 'left',
+			tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+			element: document.getElementById(yaxis)
+		} );
+
+		new Rickshaw.Graph.Axis.Time({
+			graph: graph[index],
+			timeFixture: new Rickshaw.Fixtures.Time.Local()
+		});
+
+		graph[index].render()
+		
+		new Rickshaw.Graph.HoverDetail({
+			graph: graph[index],
+			formatter: function (series, x, y) {
+				return content = "<span class='date'>"+d3.time.format("%Y-%m-%d %H:%M:%S")(new Date(x*1000)) +"</span><br/>"+y
+			}
+		});
+
+		$.each(data, function(i, point) {                                                                         
+			x = {data: point.y} 
+			graph[index].series.addData(x) //{name: gon.metric[index], data: point.y})
+		})
+		graph[index].render()
+
 		unrenderWaiting();
 		renderSlider();
-        }) 
-	
-
+	}) 
 }
+
 function updateRickshaw(){ 
 	id = setInterval(function() { 		
 		now = parseInt(Date.now()/1000)
@@ -120,7 +72,6 @@ function updateRickshaw(){
 	, gon.step*1000);
 	return id
 }
-
 
 var complete = 0;
 function renderSlider() { 
