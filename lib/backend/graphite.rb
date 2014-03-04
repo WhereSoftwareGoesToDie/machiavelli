ee533ff56b8a427998db0e8b59937302416e147f
Katie McLaughlin <katie@anchor.net.au>
2014-03-05 10:26:39 +1100

    Dynamically change http/https get function for graphite
    Needs to use net/http for parsing of non-standard system error message (using
    open-uri just raises an error without the ability to check the resulting
    website for the actual error message).

--- lib/backend/graphite.rb
+++ lib/backend/graphite.rb
@@ -1,6 +1,6 @@
 # Required config/settings.yml > backend > settings parameters: 
 #  url - the graphite instance url. 
-
+require 'net/http'
 
 # Here be graphite hackery
 class Backend::Graphite < Backend::GenericBackend
@@ -19,17 +19,17 @@ class Backend::Graphite < Backend::GenericBackend
 	end
 
 	def get_metric m, start=nil, stop=nil, step=nil
-		uri = 	@base_url + 
+		url = 	@base_url + 
 			"/render?target=" + m + 
 			"&from=#{start}" +
 			"&until=#{stop}" +
 			"&format=json"
 
 		# Because why cook when you can create?
-		uri.gsub!(m, "summarize(#{m},'#{step}sec','avg')") if step
+		url.gsub!(m, "summarize(#{m},'#{step}sec','avg')") if step
 
 		begin
-			stats = get_json uri
+			stats = get_json url
 		rescue ArgumentError => e
 			raise Backend::Error, "Graphite Exception raised: #{e}"
 		rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
@@ -47,15 +47,24 @@ class Backend::Graphite < Backend::GenericBackend
 		metric
 	end
 
-	def get_json uri
-		result = Net::HTTP.get URI.parse(uri)
+	def get_json url
+		uri = URI.parse(url)
+
+		http = Net::HTTP.new(uri.host, uri.port)
+
+		if uri.is_a? URI::HTTPS then
+			http.use_ssl = true
+			http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+		end
+
+		result = http.get uri.request_uri
 
 		# Because asking for json return html if there's an error. 
-		if result.include? "Exception"
-			raise ArgumentError, result.split("\n").select{|i| i[/^Exception/]}.join("\n").gsub("&#39;","'")
+		if result.body.include? "Exception"
+			raise ArgumentError, result.body.split("\n").select{|i| i[/^Exception/]}.join("\n").gsub("&#39;","'")
 		end
 		
-		JSON.parse(result)
+		JSON.parse(result.body)
 	end
 end
 
