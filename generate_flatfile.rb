b607ee8b0a699726b340de2abc2e86e3d513e8d3
Katie McLaughlin <katie@anchor.net.au>
2014-03-10 09:42:23 +1100

    Update generated_flatfile to allow parameter based step
    Default to 1 if no value
    Change travis.yml setting for testing

--- generate_flatfile.rb
+++ generate_flatfile.rb
@@ -1,12 +1,11 @@
-file = "public/flatfile_15s.csv"
 
 datapoints = 600
-step = 15
+step = ARGV[0].to_i || 15
+step = 1 if step == 0
 
+file = "public/flatfile_#{step}s.csv"
 stop = Time.now().to_i
 start = stop - (datapoints * step)
-
-
 data = []
 (start..stop).step(step).each do |p|
 	data.push "#{p},#{rand(100)}"
@@ -16,4 +15,4 @@ File.open(file, "w") do |f|
 	f.puts data
 end
 
-
+puts "Generated #{file}"
