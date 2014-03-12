6457fbb381beea7558dc156229a4f659ae814f40
Katie McLaughlin <katie@anchor.net.au>
2014-03-12 15:50:28 +1100

    Add local specs for simple_endpoint
    designated local because cannot be run within travis, without sinatra hackery
    that I can't seem to make happen

---
+++ spec/local/simple_endpoint_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "Simple backend", :js => true do
+
+        type = "Simple"
+	name = "Clizia"
+	host = "http://localhost:4567"
+	metric = "#{type}:#{name}"
+	
+	before :each do
+		add_config "backends: [{ type: '#{type}', settings: { url: '#{host}'}}]"
+		test_config type
+	end
+
+	context "refresh metrics" do
+		include_examples "refresh metrics", type
+	end
+	
+	context "standard graphs" do
+		it_behaves_like "a graph", "standard", metric
+	end
+	
+	context "stacked graphs" do
+		it_behaves_like "a graph", "stacked", metric
+        end
+	
+	context "horizon graphs" do
+		it_behaves_like "a graph", "horizon", metric
+        end
+
+end
+
