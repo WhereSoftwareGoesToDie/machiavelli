c26accca53764a98fb1176bc73667bb82a9a32b2
Katie McLaughlin <katie@anchor.net.au>
2014-03-12 15:23:51 +1100

    Make generate_flatfile methodized and self-generating

--- generate_flatfile.rb
+++ generate_flatfile.rb
@@ -1,18 +1,21 @@
+def generate step=15, datapoints=600
+	step = 1 if step == 0
 
-datapoints = 600
-step = ARGV[0].to_i || 15
-step = 1 if step == 0
+	file = "public/flatfile_#{step}s.csv"
+	stop = Time.now().to_i
+	start = stop - (datapoints * step)
+	data = []
+	(start..stop).step(step).each do |p|
+		data.push "#{p},#{rand(100)}"
+	end
 
-file = "public/flatfile_#{step}s.csv"
-stop = Time.now().to_i
-start = stop - (datapoints * step)
-data = []
-(start..stop).step(step).each do |p|
-	data.push "#{p},#{rand(100)}"
+	File.open(file, "w") do |f|
+		f.puts data
+	end
+
+	puts "Generated #{file}"
 end
 
-File.open(file, "w") do |f|
-	f.puts data
-end
+generate 1
+generate 15
 
-puts "Generated #{file}"
