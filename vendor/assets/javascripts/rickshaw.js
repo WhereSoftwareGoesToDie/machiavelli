9e8f2947b6220dfc41bd5b2ae3bf8adb14e9067d
Katie McLaughlin <katie@anchor.net.au>
2014-03-20 12:33:16 +1100

    Update Rickshaw.Fixture.Number.js to glasnt/rickshaw:round-y-axis-tick-label version

--- vendor/assets/javascripts/rickshaw.js
+++ vendor/assets/javascripts/rickshaw.js
@@ -1135,17 +1135,41 @@ Rickshaw.namespace('Rickshaw.Fixtures.Number');
 
 Rickshaw.Fixtures.Number.formatKMBT = function(y) {
         var abs_y = Math.abs(y);
-        var precision = 2;
+        if (abs_y >= 1000000000000)   { return y / 1000000000000 + "T" }
+        else if (abs_y >= 1000000000) { return y / 1000000000 + "B" }
+        else if (abs_y >= 1000000)    { return y / 1000000 + "M" }
+        else if (abs_y >= 1000)       { return y / 1000 + "K" }
+        else if (abs_y < 1 && y > 0)  { return y.toFixed(2) }
+        else if (abs_y === 0)         { return 0 }
+        else                      { return y }
+};
+
+
+Rickshaw.Fixtures.Number.formatKMBT_round = function(y) { 
+        var abs_y = Math.abs(y);
+	var precision = 2;
         if (abs_y >= 1000000000000)   { return (y / 1000000000000).toFixed(precision) + "T" }
         else if (abs_y >= 1000000000) { return (y / 1000000000).toFixed(precision) + "B" }
         else if (abs_y >= 1000000)    { return (y / 1000000).toFixed(precision) + "M" }
         else if (abs_y >= 1000)       { return (y / 1000).toFixed(precision) + "K" }
         else if (abs_y < 1 && y > 0)  { return y.toFixed(precision) }
-        else if (abs_y === 0)         { return '' }
-        else                      { return y }
-};
+        else if (abs_y === 0)         { return 0 }
+        else                      { return y.toFixed(precision) }
+}; 
 
 Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
+    var abs_y = Math.abs(y);
+    if (abs_y >= 1125899906842624)  { return y / 1125899906842624 + "P" }
+    else if (abs_y >= 1099511627776){ return y / 1099511627776 + "T" }
+    else if (abs_y >= 1073741824)   { return y / 1073741824 + "G" }
+    else if (abs_y >= 1048576)      { return y / 1048576 + "M" }
+    else if (abs_y >= 1024)         { return y / 1024 + "K" }
+    else if (abs_y < 1 && y > 0)    { return y.toFixed(2) }
+    else if (abs_y === 0)           { return 0 }
+    else                        { return y }
+};
+
+Rickshaw.Fixtures.Number.formatBase1024KMGTP_round = function(y) { 
     var abs_y = Math.abs(y);
     var precision = 2;
     if (abs_y >= 1125899906842624)  { return (y / 1125899906842624).toFixed(precision) + "P" }
@@ -1153,9 +1177,9 @@ Rickshaw.Fixtures.Number.formatBase1024KMGTP = function(y) {
     else if (abs_y >= 1073741824)   { return (y / 1073741824).toFixed(precision) + "G" }
     else if (abs_y >= 1048576)      { return (y / 1048576).toFixed(precision) + "M" }
     else if (abs_y >= 1024)         { return (y / 1024).toFixed(precision) + "K" }
-    else if (abs_y < 1 && y > 0)    { return y.toFixed(precision) }
-    else if (abs_y === 0)           { return '' }
-    else                        { return y }
+    else if (abs_y < 1 && y > 0)    { return y.toFixed(2) }
+    else if (abs_y === 0)           { return 0 }
+    else                      { return y.toFixed(precision) }
 };
 
 Rickshaw.namespace("Rickshaw.Color.Palette");
