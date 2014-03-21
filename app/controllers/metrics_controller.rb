1ae1e7a3acd99a6810e3b87cbd81c04c78df71b4
Katie McLaughlin <katie@anchor.net.au>
2014-03-21 15:15:31 +1100

    Define list function for metrics (searching for metric names)

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,15 +1,18 @@
 require 'uri'
 
 class MetricsController < ApplicationController
-	helper :all
 	
+	include Layouts::ApplicationLayoutHelper
+
+	helper_method :init_backend
+
 	START = 60*60*3
 	STOP  = 0
 	STEP = 10
 # GET
 	def get
 		unless params[:metric] then
-			render json: available_metrics
+			render json: {error: "must provide a metric"}
 			return
 		end
 		m = params[:metric]
@@ -19,11 +22,6 @@ class MetricsController < ApplicationController
 		stop   = (params[:stop] || now - STOP).to_i
 		step   = (params[:step] || STEP).to_i
 
-		unless available_metrics.include? URI.decode(m) then
-			render json: {error: "Metric '#{m}' not in list of available metrics"}
-			return
-		end
-
 		begin
 			metric = get_metric(m, start, stop, step)
 			render json: metric	
@@ -31,6 +29,29 @@ class MetricsController < ApplicationController
 			render json: { error: e.to_s } 
 			return
 		end
+	end
+
+	def list 
+		backend = params[:backend]
+		search = params[:q] || "*"
+
+		unless backend
+			render json: {error: "must provide a backend origin"}
+			return
+		end
+
+		begin
+			list = (init_backend backend).search_metric_list search
+			if params[:callback] then
+				render json: "#{params[:callback]}({metrics:#{list.map{|x| {metric: x}}.to_json}});"
+			else
+				render json: list
+			end
+		rescue Backend::Error => e
+			render json: { error: e.to_s } 
+			return
+		end
+		
 	end
 	
 # Functions
@@ -41,7 +62,7 @@ class MetricsController < ApplicationController
 
 	def get_metric m, start, stop, step
 		metric = m.split(":").last
-		(backend m).get_metric metric, start, stop, step
+		(init_backend m).get_metric metric, start, stop, step
 	end
 
 end
