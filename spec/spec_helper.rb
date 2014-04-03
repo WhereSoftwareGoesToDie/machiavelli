e16161f95d912b01b522a68811119c92950ef440
Katie McLaughlin <katie@anchor.net.au>
2014-04-03 11:20:59 +1100

    only really need waiting for when using local slower endpoints, not travis-style stuff

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,16 +19,10 @@ require 'capybara/webkit'
 require 'binding_of_caller'
 require 'redis'
 
-
 TEMP_YML = "temp_settings.yml"
 REDIS_METRIC_KEY = "Machiavelli.Metrics"
 
-if ENV["TRAVIS"] then
-	Capybara.default_wait_time = 30
-else 
-	Capybara.default_wait_time = 20
-	Capybara.server_port = 31337
-end
+Capybara.default_wait_time = 10
 
 Capybara.javascript_driver = :webkit unless ENV["BROWSER"] == "firefox"
 
@@ -40,7 +34,8 @@ RSpec.configure do |config|
 end
 
 shared_examples 'a graph' do |metric|
-	["10min","1h","3h","1d","1w","2w"].each do |t|
+#	["10min","1h","3h","1d","1w","2w"].each do |t|
+	["10min","3h","1w"].each do |t|
 		time_css_button metric, "standard", t, ["#multi_slider","#chart_0",".x_tick"]
 		time_css_button metric, "stacked",  t, [".rickshaw_graph","#chart_container",".legend-indent",".y_axis"]
 		time_css_button metric, "horizon",  t, [".horizon","#horizon_graph",".axis"]
@@ -112,13 +107,16 @@ end
 def wait_for_ajax *args
 
         wait_time = Capybara.default_wait_time
-        counter = 0
 
-	while page.evaluate_script("$.active").to_i > 0
-                counter += 0.1
-                sleep(0.1)
-                if counter >= wait_time then
-                        raise "AJAX request took longer than #{wait_time} seconds. Args used: #{args}"
-                end
+	unless ENV["TRAVIS"] then
+		counter = 0
+
+		while page.evaluate_script("$.active").to_i > 0
+			counter += 0.1
+			sleep(0.1)
+			if counter >= wait_time then
+				raise "AJAX request took longer than #{wait_time} seconds. Args used: #{args}"
+			end
+		end
         end
 end
