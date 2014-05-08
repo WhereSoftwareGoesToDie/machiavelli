ca9998ceac4d6fdbc479a486a94506dd5fd42a80
Katie McLaughlin <katie@glasnt.com>
2014-05-08 13:53:41 +1000

    Add changer for time scales. Save route changes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Machiavelli::Application.routes.draw do
 
   post "submit" => "graphs#submit", as: "submit"
   post "stop_time" => "graphs#stop_time", as: "stop_time"
+  post "absolute_time" => "graphs#absolute_time", as: "absolute_time"
 
   get "refresh" => "graphs#refresh", as: "refresh"
 
