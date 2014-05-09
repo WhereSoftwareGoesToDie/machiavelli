17d6e27cc75f762d1b264783198eb6b7e162406a
Katie McLaughlin <katie@glasnt.com>
2014-05-09 16:09:41 +1000

    Pull all files in any nested folder under lib/ at start time

--- config/initializers/module_load.rb
+++ config/initializers/module_load.rb
@@ -1,4 +1,4 @@
 # Require any module we have defined in the library
-Dir[File.dirname(__FILE__) + '/lib/**/*.rb'].each do |f| 
+Dir.glob("lib{,/*/**}/*.rb").each do |f| 
 	require File.join(Rails.root, f)
 end
