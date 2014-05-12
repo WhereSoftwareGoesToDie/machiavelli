637a6b41dc5607a3d841e84a086b62cdbbce3a1f
Katie McLaughlin <katie@glasnt.com>
2014-05-12 10:42:30 +1000

    use recursive call for loading all the library files

--- config/initializers/module_load.rb
+++ config/initializers/module_load.rb
@@ -1,4 +1,17 @@
 # Require any module we have defined in the library
-Dir.glob("lib{,/*/**}/*.rb").each do |f| 
-	require File.join(Rails.root, f)
+
+# Recusive call to ensure all child directories, real and symlinked, are traversed.
+def all_files_under(*paths)
+  paths.flatten!
+  paths.map! { |p| Pathname.new(p) }
+  files = paths.select { |p| p.file? }
+  (paths - files).each do |dir|
+    files << all_files_under(dir.children)
+  end
+  files.flatten
 end
+
+all_files_under("lib").each do |f|
+	require File.join(Rails.root, f.to_s)
+end
+
