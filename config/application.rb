20f1ade51e835d9931c2ff54ac843c8d46971919
Katie McLaughlin <katie@glasnt.com>
2014-07-31 10:31:42 +1000

    test rm action_mailer|controller

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "action_controller/railtie"
-require "action_mailer/railtie"
+#require "action_controller/railtie"
+#require "action_mailer/railtie"
 require "sprockets/railtie"
 
 
