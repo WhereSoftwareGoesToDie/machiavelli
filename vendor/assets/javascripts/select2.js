be2da5ca13b937e85d9f6620e7320c84093f282b
Katie McLaughlin <katie@glasnt.com>
2014-08-01 18:06:21 +1000

    have loading more results as unobtrusive spinner instead of a new line item
    formatLoadMore requires text and not a space to work. change font colour to remove dot from listing?

--- vendor/assets/javascripts/select2.js
+++ vendor/assets/javascripts/select2.js
@@ -3334,7 +3334,7 @@ the specific language governing permissions and limitations under the Apache Lic
         formatInputTooShort: function (input, min) { var n = min - input.length; return "Please enter " + n + " or more character" + (n == 1? "" : "s"); },
         formatInputTooLong: function (input, max) { var n = input.length - max; return "Please delete " + n + " character" + (n == 1? "" : "s"); },
         formatSelectionTooBig: function (limit) { return "You can only select " + limit + " item" + (limit == 1 ? "" : "s"); },
-        formatLoadMore: function (pageNumber) { return "Loading more results…"; },
+        formatLoadMore: function (pageNumber) { return "."; },
         formatSearching: function () { return "Searching…"; },
         formatDropdownHeader: function () { return undefined; },
         formatDropdownFooter: function () { return undefined; },
