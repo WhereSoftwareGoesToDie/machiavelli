64627ee892ca08c50a3a7dfcbcebe76b2fbfad6a
Katie McLaughlin <katie@glasnt.com>
2014-09-05 15:20:37 +1000

    Create a Error Store, Default Source object in case of error
    If an invalid origin is used, made a metric with settings of the input,
    but use the new ErrorStore to return data when things fail.
    
    Since the UI relies on metrics being present, even if they hold invalid
    data, don't error out until after the metric has been put to html, and
    only error on the getting of metric information

---
+++ lib/errorstore.rb
@@ -0,0 +1,11 @@
+class Errorstore < Store
+        def get_metric _,_,_,_
+                raise Store::Error, "Invalid origin: '#{@origin_id}' not defined in settings"
+        end   
+        def get_metric_url _,_,_,_
+                ""
+        end     
+        def get_metric_list
+                []
+        end
+end
